541372608facddfe6b0e5a4042162f650c11b7f9
Brapl <bray.paul@live.fr>
2022-03-05 18:13:20 +0100

    Initial commit

---
+++ Unity/BTW/Assets/Script/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void SoloButton()
+    {
+
+    }
+
+    public void MultiButton()
+    {
+
+    }
+
+    public void SiteButton()
+    {
+
+    }
+
+    public void DidaButton()
+    {
+        SceneManager.LoadScene("Didactitiel");
+    }
+
+    public void OptionsButton() 
+    {
+
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+}
