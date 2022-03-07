3feca290796534c24d2965b2b8587d1ce0313563
Brapl <bray.paul@live.fr>
2022-03-07 17:00:40 +0100

    Fin Réinitialisation

---
+++ Unity/BTW/Assets/Script/OptionsMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void DropReso()
+    {
+
+    }
+
+    public void ToogleFS()
+    {
+
+    }
+
+    public void SliderSound(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+}
