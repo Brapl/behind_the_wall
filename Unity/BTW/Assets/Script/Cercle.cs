766e6c2b460b009de28f78a2656c76f0c545bb37
Jean-Jules SUN <jean-jules.sun@epita.fr>
2022-04-22 22:11:20 +0200

    ajout des scripts sur les objets et interactUI

---
+++ Unity/BTW/Assets/Script/Cercle.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cercle : MonoBehaviour
+{
+    public bool isInRange;
+    public Text interactUI;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange == true && Input.GetKeyDown(KeyCode.F))
+        {
+            return;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            interactUI.enabled = false;
+        }
+    }
+}
