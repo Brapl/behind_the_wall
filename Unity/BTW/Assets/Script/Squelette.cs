ce4b82b1b8da003c8f39534c7297ffb88f868ee5
Jean-Jules SUN <jean-jules.sun@epita.fr>
2022-04-26 21:45:10 +0200

    PNJ Squelette

---
+++ Unity/BTW/Assets/Script/Squelette.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Squelette : MonoBehaviour
+{
+	public bool isInRange;
+	public DialogueUI dialogue;
+    public Text interactUI;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        interactUI = GameObject.FindGameObjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange == true && Input.GetKeyDown(KeyCode.F))
+        {
+            TriggerDialogue();
+        }
+    }
+	private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.enabled = true;
+            isInRange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+            interactUI.enabled = false;
+        }
+    }
+	void TriggerDialogue()
+    {
+        DialogueManager.instance.StartDialogue(dialogue);
+    }
+}
